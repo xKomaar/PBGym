9f8a6b2bb8167170558916e3236a644442e22c94
xKomaar <jakubo148@gmail.com>
2024-07-11 22:36:02 +0200

    Add mass testing functionality and reading .env file

--- src/main/java/pl/pbgym/service/auth/JwtService.java
+++ src/main/java/pl/pbgym/service/auth/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +17,8 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-
-    private static final String SECRET_KEY = System.getenv("JWT_SECRET_KEY");
+    @Value("${env.JWT_SECRET_KEY}")
+    private String SECRET_KEY;
     public String extractEmail(String jwt) {
         return extractClaim(jwt, Claims::getSubject);
     }
