7963759fedeb01a99324268c9762b6ddd02a9d6a
xKomaar <jakubo148@gmail.com>
2024-09-04 23:06:16 +0200

    adjust tests

--- src/test/java/pl/pbgym/auth/MemberAuthenticationControllerTest.java
+++ src/test/java/pl/pbgym/auth/MemberAuthenticationControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import pl.pbgym.dto.auth.PostAddressRequestDto;
--- src/test/java/pl/pbgym/user/trainer/TrainerControllerTest.java
+++ src/test/java/pl/pbgym/user/trainer/TrainerControllerTest.java
@@ -9,8 +9,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -35,6 +37,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles(profiles = "test")
+@Profile("test")
 public class TrainerControllerTest {
 
     @Autowired
