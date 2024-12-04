f0d407341aee88a52d1e0815407fca06667b1dc0
xKomaar <jakubo148@gmail.com>
2024-12-04 19:40:45 +0100

    Add test loggers

--- src/main/java/pl/pbgym/service/user/trainer/GroupClassService.java
+++ src/main/java/pl/pbgym/service/user/trainer/GroupClassService.java
@@ -19,7 +19,6 @@ import pl.pbgym.repository.user.trainer.TrainerRepository;
 import pl.pbgym.service.user.member.MemberService;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -46,14 +45,14 @@ public class GroupClassService {
 
     public List<GetGroupClassResponseDto> getAllUpcomingGroupClasses() {
         logger.info("Pobieranie wszystkich nadchodzących zajęć grupowych.");
-        return mapGroupClassesToUpcomingClasses(groupClassRepository.findAll()).stream().map(this::mapGroupClassToDto).toList();
-        //return groupClassRepository.findAllUpcomingGroupClasses(LocalDateTime.now()).stream().map(this::mapGroupClassToDto).toList();
+        logger.info(groupClassRepository.findAllUpcomingGroupClasses(LocalDateTime.now()).stream().map(this::mapGroupClassToDto).toList().toString());
+        return groupClassRepository.findAllUpcomingGroupClasses(LocalDateTime.now()).stream().map(this::mapGroupClassToDto).toList();
     }
 
     public List<GetGroupClassResponseDto> getAllHistoricalGroupClasses() {
         logger.info("Pobieranie wszystkich historycznych zajęć grupowych.");
-        return mapGroupClassesToHistoricalClasses(groupClassRepository.findAll()).stream().map(this::mapGroupClassToDto).toList();
-        //return groupClassRepository.findAllHistoricalGroupClasses(LocalDateTime.now()).stream().map(this::mapGroupClassToDto).toList();
+        logger.info(groupClassRepository.findAllHistoricalGroupClasses(LocalDateTime.now()).stream().map(this::mapGroupClassToDto).toList().toString());
+        return groupClassRepository.findAllHistoricalGroupClasses(LocalDateTime.now()).stream().map(this::mapGroupClassToDto).toList();
     }
 
     public List<GetGroupClassResponseDto> getAllUpcomingGroupClassesByTrainerEmail(String email) {
@@ -252,21 +251,8 @@ public class GroupClassService {
         });
     }
 
-    protected List<GroupClass> mapGroupClassesToUpcomingClasses(List<GroupClass> groupClassesToMap) {
-        return groupClassesToMap.stream()
-                .filter(groupclass -> !isGroupClassHistorical(groupclass))
-                .toList();
-    }
-
-    protected List<GroupClass> mapGroupClassesToHistoricalClasses(List<GroupClass> groupClassesToMap) {
-        return groupClassesToMap.stream()
-                .filter(this::isGroupClassHistorical)
-                .toList();
-    }
-
     protected boolean isGroupClassHistorical(GroupClass groupClass) {
         LocalDateTime now = LocalDateTime.now();
-        logger.info("NOW: " + now);
         return groupClass.getDateStart().isBefore(now) || groupClass.getDateStart().isEqual(now);
     }
 
